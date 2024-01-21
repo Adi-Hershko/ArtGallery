36726ff9155f825a88fcfe3cbe8de7508ebb6bac
Adi-Hershko <adi.hershko7@gmail.com>
2024-01-21 13:14:26 +0200

    Deleted backend.py and modified post_controller name

--- app/application.py
+++ app/application.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from app.routes.user_controller import user_controller_router
-from app.routes.post_controller import post_controller
+from app.routes.post_controller import post_controller_router
 
 app = FastAPI()
 
 app.include_router(user_controller_router)
-app.include_router(post_controller)
+app.include_router(post_controller_router)
