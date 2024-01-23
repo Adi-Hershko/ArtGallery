fbed5e6fdd0b5a7017578204374db7ebf388a626
Adi-Hershko <adi.hershko7@gmail.com>
2024-01-24 00:50:29 +0200

    Fixed the sign-in method, changed the typeof password in the users DB into LargeBinary instead of string

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,5 +1,6 @@
 import bcrypt
-from app.dals.user_dal import add_user
+from app.dals.user_dal import add_user, find_user
+from app.exceptions import UserNotFoundException, PasswordNotMatchException
 
 async def create_user(username:str, password:str) -> bool:
     salt = bcrypt.gensalt()
@@ -8,8 +9,19 @@ async def create_user(username:str, password:str) -> bool:
     print("Hashed password: ", hashed_password)
     await add_user(username, hashed_password)
     return True
-    
+
+
 async def validate_user(username: str, password: str):
-    pass
-    # hashed_password = get_hashed_password(username, password)
-    # return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
+    try:
+        user = await find_user(username)
+        if isinstance(user,UserNotFoundException):            
+            return UserNotFoundException(f"User '{username}' not found.")
+        print("User: ", user)
+        print("Username: ", user.username)        
+        if bcrypt.checkpw(password.encode("utf-8"), user.password):
+            return True
+        else:
+            return PasswordNotMatchException(f"Password does not match.")
+    except Exception as e:
+        print(f"Error inside validate_user: {e}")
+        
